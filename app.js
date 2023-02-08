4a2a1a38babca6b014559a5c84cf5ac0f972d6cc
philippemaia <philippe.m.maia@hotmail.com>
2023-02-08 00:49:47 -0300

    Alterando o app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando com o banco de dados");
+// Mais um console log para teste
+console.log("testando alterações");
