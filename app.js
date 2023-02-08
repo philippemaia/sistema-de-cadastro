eb41c7403bba7d498030ee0367823c682c1b5d30
philippemaia <philippe.m.maia@hotmail.com>
2023-02-08 00:51:27 -0300

    adicionando mais um console  no app js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando com o banco de dados");
 // Mais um console log para teste
 console.log("testando alterações");
+console.log("testando alterações 2");
