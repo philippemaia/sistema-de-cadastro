ac86c926f2b15c971811badaedb65e79a939ebe7
philippemaia <philippe.m.maia@hotmail.com>
2023-02-08 01:18:36 -0300

    voltando para a versão anterior app js

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando com o banco de dados");
 // Mais um console log para teste
 console.log("testando alterações");
-console.log("testando alterações 2");
